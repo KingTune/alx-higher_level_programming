a34bbd2e663de76bc57065246f2672f00e04c812
KingTune <tunechikalifa365@gmail.com>
2022-10-18 17:03:09 +0000

    low memory

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-"""
-
-This is a module that containts a clas that avoids
-dynmaically created attributes
-
-"""
+"""Locked class module"""
 
 
 class LockedClass:
-    __slots__ = ['first_name']
-
-    def __init__(self):
-        """ Init method """
-        pass
+    """Locked class using slots to save memory"""
+    __slots__ = "first_name",
