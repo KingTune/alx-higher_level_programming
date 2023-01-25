7797d924a86a34595f7fc7874d17c3a8c23adc35
KingTune <tunechikalifa365@gmail.com>
2023-01-25 16:36:08 +0000

    hbtn

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
-if __name__ == '__main__':
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(
-          type(r.text), r.text))
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
