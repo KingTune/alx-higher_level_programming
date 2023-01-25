53d4be807a233dace1b40bb869b86666c25c68b6
KingTune <tunechikalifa365@gmail.com>
2023-01-25 16:38:17 +0000

    hbtn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status with urlib"""
-from urllib import request
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
 
 if __name__ == '__main__':
-    req = request.Request("https://intranet.hbtn.io/status")
-    with request.urlopen(req) as response:
-        r = response.read()
-        s = "Body response:\n\t- type: {}\n\t- content: {}"
-        s += "\n\t- utf8 content: {}"
-        print(s.format(type(r), r, r.decode('utf-8')))
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
